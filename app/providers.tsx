0417a8a457e789e60fd3c6d44534bece5085c366
SRIKRISHNA S KASHYAP <skashyapsri04@gmail.com>
2025-09-03 09:36:50 +0530

    :sparkles: (feat) : Updated posthog config

--- app/providers.tsx
+++ app/providers.tsx
@@ -19,13 +19,14 @@ export default function Providers({ children }: { children: React.ReactNode }) {
 	);
 
 	useEffect(() => {
-		posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-			api_host: "/ingest",
-			ui_host: "https://us.posthog.com",
-			defaults: '2025-05-24',
-			capture_exceptions: true, // This enables capturing exceptions using Error Tracking
-			debug: process.env.NODE_ENV === "development",
-		});
+		if (process.env.NEXT_PUBLIC_POSTHOG_KEY)
+			posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+				api_host: "/ingest",
+				ui_host: "https://us.posthog.com",
+				defaults: '2025-05-24',
+				capture_exceptions: true, // This enables capturing exceptions using Error Tracking
+				debug: process.env.NODE_ENV === "development",
+			});
 	}, []);
 
 	return (
